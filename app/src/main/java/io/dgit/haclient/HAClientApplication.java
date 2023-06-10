763d3e2104b9681f6faf95a7cd81c46b13674f8b
dg-it <52246671+dg-it@users.noreply.github.com>
2023-06-10 20:16:56 +0200

    Setting up basis of app using spring boot

---
+++ app/src/main/java/io/dgit/haclient/HAClientApplication.java
@@ -0,0 +1,13 @@
+package io.dgit.haclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HAClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HAClientApplication.class, args);
+    }
+
+}
---
+++ app/src/test/java/io/dgit/haclient/HAClientApplicationTest.java
@@ -0,0 +1,13 @@
+package io.dgit.haclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HAClientApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
